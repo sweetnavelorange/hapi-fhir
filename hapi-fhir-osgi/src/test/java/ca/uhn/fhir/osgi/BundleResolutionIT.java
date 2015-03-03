6ecebc7efe89a4a3cdee615be6a39fa89f222600
James Butler <james.butler@orionhealth.com>
2015-03-03 13:16:04 +1300

    Use maven to build list of bundles to be included in OSGi integration test container

--- hapi-fhir-osgi/src/test/java/ca/uhn/fhir/osgi/BundleResolutionIT.java
+++ hapi-fhir-osgi/src/test/java/ca/uhn/fhir/osgi/BundleResolutionIT.java
@@ -5,6 +5,13 @@ import static org.ops4j.pax.exam.CoreOptions.*;
  
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.parser.IParser;
+import java.util.ArrayList;
+import java.util.List;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
 import javax.inject.Inject;
 import org.junit.Test;
 import org.junit.Before;
@@ -25,23 +32,23 @@ public class BundleResolutionIT {
     private BundleContext bundleContext;
  
     @Configuration
-    public Option[] config() {
- 
-        return options(
-            bundle("file:target/hapi-fhir-osgi-0.9-SNAPSHOT.jar"),
-            mavenBundle("javax.json", "javax.json-api", "1.0"),
-            mavenBundle("org.glassfish", "javax.json", "1.0.4"),
-            mavenBundle("org.apache.commons", "commons-lang3", "3.3.2"),
-            mavenBundle("commons-codec", "commons-codec", "1.10"),
-            mavenBundle("commons-io", "commons-io", "2.4"),
-            mavenBundle("org.slf4j", "slf4j-api", "1.7.9"),
-	    mavenBundle("org.slf4j", "slf4j-simple", "1.7.9").noStart(),
-            mavenBundle("org.apache.httpcomponents", "httpcore-osgi", "4.3.3"),
-            mavenBundle("org.apache.httpcomponents", "httpclient-osgi", "4.3.6"),
-            mavenBundle("org.codehaus.woodstox", "stax2-api", "3.1.4"),
-            mavenBundle("org.apache.felix", "org.apache.felix.http.servlet-api", "1.0.0"),
-            junitBundles()
-            );
+    public Option[] config() throws IOException {
+        final List<Option> options = new ArrayList<Option>();
+	options.add(junitBundles());
+	final DirectoryStream<Path> hapiBundleDirectory = Files.newDirectoryStream(Paths.get("target"), "hapi-fhir-osgi-*.jar");
+	// Should match a single file
+	options.add(bundle("file:" + hapiBundleDirectory.iterator().next().toString()));
+	final DirectoryStream<Path> depsDirectory = Files.newDirectoryStream(Paths.get("target/deps"));
+	for (Path depPath: depsDirectory) {
+		final String depName = depPath.toString();
+		if (depName.contains("slf4j-simple")) {
+			// This is a fragment bundle, which cannot be started
+			options.add(bundle("file:" + depPath.toString()).noStart());
+		} else {
+			options.add(bundle("file:" + depPath.toString()));
+		}
+	}
+        return options.toArray(new Option[options.size()]);
     }
 
     @Before
